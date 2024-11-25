eabcf5c72f23393df8420d005bfa12c4225447e0
Changyu Moon <121847433+window9u@users.noreply.github.com>
2024-11-25 19:09:41 +0900

    feat forwarding

--- static/js/client/client.js
+++ static/js/client/client.js
@@ -15,6 +15,7 @@ export default class Client {
         this.socket = null;
         this.requestId = 0;
         this.pendingRequests = {};
+        this.messageHandlers = {};
         this.mediaStream = null;
     }
 
@@ -33,7 +34,15 @@ export default class Client {
                 if (requestId && this.pendingRequests[requestId]) {
                     this.pendingRequests[requestId](data);
                     delete this.pendingRequests[requestId];
+                } else {
+                    const type = data.type;
+                    if (this.messageHandlers[type]) {
+                        this.messageHandlers[type](data);
+                    } else {
+                        console.warn("Unhandled message type:", type);
+                    }
                 }
+
             };
         });
     }
@@ -132,7 +141,16 @@ export default class Client {
 
     async Fetch() {
         try {
-            const connection = new RTCPeerConnection();
+            const connection = new RTCPeerConnection({
+                iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+            });
+            connection.addTransceiver('video');
+
+            connection.onicecandidate = (event) => {
+                if (event.candidate) {
+                    const offer =  connection.createOffer();
+                }
+            };
 
             connection.ontrack = (event) => {
                 this.video.srcObject = event.streams[0];
@@ -142,20 +160,19 @@ export default class Client {
 
             const offer = await connection.createOffer();
             await connection.setLocalDescription(offer);
+            console.log("Generated Offer SDP:", offer.sdp);
 
-            const response = await this.send({
+            await this.send({
                 Type: MESSAGE_TYPES.FETCH,
-                SDP: offer.sdp
-            });
-
-            await connection.setRemoteDescription(new RTCSessionDescription({
-                type: 'answer',
-                sdp: response.SDP
-            }));
-
-            console.log("Fetch completed.");
+                sdp: offer.sdp
+            }).then(async (response) => {
+                await connection.setRemoteDescription(new RTCSessionDescription({
+                    type: 'answer',
+                    sdp: response.sdp
+                }));
+            })
         } catch (error) {
-            console.error("Error in Fetch:", error);
+            console.error("Error in Pull:", error);
         }
     }
 
@@ -163,6 +180,7 @@ export default class Client {
         try {
             // Pull the stream from the server to consume and watch
             const connection = new RTCPeerConnection();
+            connection.addTransceiver('video');
 
             connection.ontrack = (event) => {
                 this.video.srcObject = event.streams[0];
@@ -175,19 +193,19 @@ export default class Client {
             const offer = await connection.createOffer();
             await connection.setLocalDescription(offer);
 
-            const response = await this.send({
+            await this.send({
                 Type: MESSAGE_TYPES.FORWARD,
-                SDP: offer.sdp
-            });
-
-            await connection.setRemoteDescription(new RTCSessionDescription({
-                type: 'answer',
-                sdp: response.SDP
-            }));
+                sdp: offer.sdp
+            }).then(async (response) => {
+                await connection.setRemoteDescription(new RTCSessionDescription({
+                    type: 'answer',
+                    sdp: response.sdp
+                }));
+            })
 
             // Set up handler for 'ARRANGE' messages from the server
             this.messageHandlers[MESSAGE_TYPES.ARRANGE] = (data) => {
-                this.Arrange(data.SDP, data.FetcherID);
+                this.Arrange(data.sdp);
             };
 
             console.log("Forwarder is set up and watching the stream.");
@@ -196,7 +214,7 @@ export default class Client {
         }
     }
 
-    async Arrange(offerSDP, fetcherID) {
+    async Arrange(offerSDP) {
         try {
             // Create a new connection to the fetcher
             const connection = new RTCPeerConnection();
@@ -212,14 +230,14 @@ export default class Client {
             }));
 
             const answer = await connection.createAnswer();
-            await connection.setLocalDescription(answer);
 
             // Send the answer back to the server to forward to the fetcher
             await this.send({
-                Type: MESSAGE_TYPES.ARRANGE,
-                SDP: answer.sdp,
-                FetcherID: fetcherID
+                type: MESSAGE_TYPES.ARRANGE,
+                sdp: answer.sdp,
+                user_id: this.userID,
             });
+            await connection.setLocalDescription(answer);
 
             console.log("Arranged connection with fetcher.");
         } catch (error) {
