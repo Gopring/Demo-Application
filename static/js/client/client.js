483fdfa3e152e4102ee256e16fd64e8a699730a5
Changyu Moon <121847433+window9u@users.noreply.github.com>
2024-12-06 17:06:26 +0900

    Implement fetch and forward

--- static/js/client/client.js
+++ static/js/client/client.js
@@ -4,7 +4,8 @@ const MESSAGE_TYPES = {
     PUSH: 'PUSH',
     FORWARD: 'FORWARD',
     ARRANGE: 'ARRANGE',
-    FETCH: 'FETCH'
+    FETCH: 'FETCH',
+    EXCHANGE: 'EXCHANGE',
 };
 
 function generateShortUUID(length = 12) {
@@ -29,22 +30,6 @@ export default class Client {
         this.socket = null;
         this.mediaStream = null;
         this.connections = {};
-        this.controller=(message) => {
-            const data = JSON.parse(message.data);
-            switch (data.type) {
-                case MESSAGE_TYPES.ACTIVATE:
-                    this.ReceiveActivate(data);
-                    break;
-                case MESSAGE_TYPES.PULL:
-                    this.ReceivePull(data);
-                    break;
-                case MESSAGE_TYPES.PUSH:
-                    this.ReceivePush(data);
-                    break;
-                default :
-                    console.log("Received unknown message:", data);
-            }
-        }
     }
 
     async dial() {
@@ -55,7 +40,31 @@ export default class Client {
 
             this.socket.onerror = (err) => reject(`Failed to connect WebSocket: ${err.message}`);
 
-            this.socket.onmessage = this.controller;
+            this.socket.onmessage = (message) => {
+                const data = JSON.parse(message.data);
+                switch (data.type) {
+                    case MESSAGE_TYPES.ACTIVATE:
+                        this.ReceiveActivate(data);
+                        break;
+                    case MESSAGE_TYPES.PULL:
+                        this.ReceivePull(data);
+                        break;
+                    case MESSAGE_TYPES.PUSH:
+                        this.ReceivePush(data);
+                        break;
+                    case MESSAGE_TYPES.FETCH:
+                        this.ReceiveFetch(data);
+                        break;
+                    case MESSAGE_TYPES.FORWARD:
+                        this.ReceiveForward(data);
+                        break;
+                    case MESSAGE_TYPES.EXCHANGE:
+                        this.ReceiveExchange(data);
+                        break;
+                    default :
+                        console.log("Received unknown message:", data);
+                }
+            };
         });
     }
 
@@ -167,136 +176,111 @@ export default class Client {
         }
     }
 
-    async Fetch() {
+    async ReceiveFetch(data) {
         try {
+            const connID=data.connection_id
             const connection = new RTCPeerConnection({
                 iceServers: [{urls: 'stun:stun.l.google.com:19302'}]
             });
+            this.connections[connID] = connection;
 
             connection.addTransceiver('video');
 
-            // ICE Gathering 완료 플래그
-            let isIceGatheringComplete = false;
             connection.ontrack = (event) => {
-                console.log("Received track event:", event);
-
-                // 기존의 streams 배열 대신 개별 트랙 사용
-                const track = event.track;
-
-                // 기존에 사용할 MediaStream이 없으면 새로 생성
-                if (!this.mediaStream) {
-                    this.mediaStream = new MediaStream(); // MediaStream 객체 생성
-                }
-                // MediaStream에 트랙 추가
-                this.mediaStream.addTrack(track);
-                // Video Element에 MediaStream 설정
-                this.video.srcObject = this.mediaStream;
+                const fetching=new MediaStream();
+                fetching.addTrack(event.track);
+                this.video.srcObject = fetching;
+                this.mediaStream = fetching;
                 this.video.autoplay = true;
                 this.video.controls = true;
-                console.log("MediaStream updated with track:", this.mediaStream);
             };
 
             connection.onicecandidate = (event) => {
-                if (!event.candidate) {
-                    // ICE Gathering이 완료되면 SDP를 전송
-                    console.log("ICE Gathering Complete");
-                    isIceGatheringComplete = true;
-
-                    // ICE가 포함된 SDP 전송
-                    sendSDPWithICE();
+                if (event.candidate) {
+                    console.log("Sending ICE candidate:", event.candidate);
+                    this.sendToServer(MESSAGE_TYPES.EXCHANGE, {
+                        connection_id: connID,
+                        type: 'candidate',
+                        data: event.candidate
+                    });
                 }
             };
 
-
-            // Offer 생성 및 설정
             const offer = await connection.createOffer();
             await connection.setLocalDescription(offer);
-
-            console.log("Local SDP created, waiting for ICE candidates...");
-
-            // SDP와 ICE를 포함한 메시지를 전송
-            const sendSDPWithICE = async () => {
-                if (!isIceGatheringComplete) return;
-
-                console.log("Sending SDP with ICE Candidates...");
-                console.log("Final SDP with ICE:", connection.localDescription.sdp);
-
-                await this.sendToServer({
-                    Type: MESSAGE_TYPES.FETCH,
-                    sdp: connection.localDescription.sdp // ICE 정보가 포함된 SDP
-                }).then(async (response) => {
-                    console.log("Received Answer SDP:", response.sdp);
-                    await connection.setRemoteDescription(
-                        new RTCSessionDescription({
-                            type: 'answer',
-                            sdp: response.sdp
-                        })
-                    );
-                });
-            };
+            this.sendToServer(MESSAGE_TYPES.FORWARD, {
+                connection_id: connID,
+                type: 'offer',
+                data: offer.sdp
+            });
         } catch (error) {
             console.error("Error in Fetch:", error);
         }
     }
 
-    async Forward() {
+    async ReceiveForward(data) {
         try {
-            // Create a new connection to the fetcher
+            const connID = data.connection_id;
             const connection = new RTCPeerConnection({
                 iceServers: [{urls: 'stun:stun.l.google.com:19302'}]
             });
+            this.connections[connID] = connection;
 
-            // Add tracks from the mediaStream to the new connection
             this.mediaStream.getTracks().forEach(track => {
                 connection.addTrack(track);
             });
 
-            let isIceGatheringComplete = false;
-
-            // Listen for ICE candidates
             connection.onicecandidate = (event) => {
-                if (!event.candidate) {
-                    // ICE Gathering 완료
-                    console.log("ICE Gathering Complete");
-                    isIceGatheringComplete = true;
-
-                    // ICE 정보가 포함된 SDP 전송
-                    sendSDPWithICE();
-                } else {
-                    console.log("ICE Candidate:", event.candidate);
+                if (event.candidate) {
+                    // console.log("Sending ICE candidate:", event.candidate);
+                    this.sendToServer(MESSAGE_TYPES.EXCHANGE, {
+                        connection_id: connID,
+                        type: 'candidate',
+                        data: event.candidate
+                    });
                 }
             };
-
-            // Set remote SDP from offer
+            // console.log("Received Offer SDP:", data.sdp);
             await connection.setRemoteDescription(new RTCSessionDescription({
                 type: 'offer',
-                sdp: offerSDP
+                sdp: data.sdp
             }));
-
-            // Create answer SDP
             const answer = await connection.createAnswer();
             await connection.setLocalDescription(answer);
-
-            console.log("Local SDP created, waiting for ICE candidates...");
-
-            // ICE Gathering 완료 후 SDP 전송
-            const sendSDPWithICE = async () => {
-                if (!isIceGatheringComplete) return;
-
-                console.log("Sending Answer SDP with ICE Candidates...");
-                console.log("Final Answer SDP with ICE:", connection.localDescription.sdp);
-
-                await this.sendToServer({
-                    type: MESSAGE_TYPES.ARRANGE,
-                    sdp: connection.localDescription.sdp, // ICE 정보 포함
-                    user_id: this.userID,
-                });
-
-                console.log("Sent Answer SDP:", connection.localDescription.sdp);
-                console.log("Arranged connection with fetcher.");
-            };
+            this.sendToServer(MESSAGE_TYPES.EXCHANGE, {
+                connection_id: data.connection_id,
+                type: 'answer',
+                data: answer.sdp
+            });
         } catch (error) {
             console.error("Error in Forward:", error);
+        }
+    }
+
+    async ReceiveExchange(data) {
+        try {
+            const connection = this.connections[data.connection_id];
+            if (connection) {
+                switch (data.type) {
+                    case 'candidate':
+                        console.log("Received ICE Candidate:", data.data);
+                        await connection.addIceCandidate(data.data);
+                        break;
+                    case 'answer':
+                        console.log("Received Answer SDP:", data.sdp);
+                        await connection.setRemoteDescription(new RTCSessionDescription({
+                            type: 'answer',
+                            sdp: data.sdp
+                        }));
+                        break;
+                    default:
+                        console.log("Unknown message type:", data.type);
+                }
+            } else {
+                console.error("Connection not found for ID:", data.connection_id);
+            }
+        } catch (error) {
+            console.error("Error in Exchange:", error);
         }
     }
 }
